b8db5c0d1488a4bc15b652ad4d3f172cdd6808a8
Sean Brown <smbrown3706@gmail.com>
2016-04-24 19:09:16 -0400

    updates

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-client = soundcloud.Client(client_id="53e3ccfd305043eb4e01b99b9cf18a37",client_secret="f29c8e8b2a7ba68a2d9fd4efa62d2e21",redirect_uri="http://brown-cloud.herokuapp.com/auth_redirect")
+client = soundcloud.Client(
+        client_id="53e3ccfd305043eb4e01b99b9cf18a37",
+        client_secret="f29c8e8b2a7ba68a2d9fd4efa62d2e21",
+        redirect_uri="http://brown-cloud.herokuapp.com/auth_redirect"
+        )
 
 user = None
 
@@ -32,7 +36,9 @@ def index():
 
 @app.route("/auth_redirect")
 def auth_redirect():
-    access_token, expires, scope, refresh_token = client.exchange_token(code=request.args.get("code")) 
+    access_token, expires, scope, refresh_token = client.exchange_token(
+            code=request.args.get("code")
+            )
     user = User(access_token, expires, scope, refresh_token, client.get("/me"))
     return redirect(url_for("/"))
 
