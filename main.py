838cf51c70a86fa7b597a70aa571a3aa27ed1091
Sean Brown <smbrown3706@gmail.com>
2016-04-26 12:40:54 -0400

    testing

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ client = soundcloud.Client(
         redirect_uri="http://brown-cloud.herokuapp.com/auth_redirect"
         )
 
-user = False 
+user = False
 
 class User:
     def __init__(self, access_token, expires, scope, refresh_token, me):
@@ -27,7 +27,7 @@ class User:
 @app.route("/")
 def index():
     logger.info('user is hello!')
-    if user:
+    if !user:
         return redirect(client.authorize_url())
     else:
         return render_template("index.html")
